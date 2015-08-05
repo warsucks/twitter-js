14f6c7c901f0bcd972abf8b4923267370d0f8ba2
Kathy Lu <kaffy.lu@gmail.com>
2015-08-05 11:56:47 -0400

    did we do stuff?

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(function(request, response)
+{
+  console.log(request.method, request.path, response.statusCode);
+  response.end();
+});
+
 app.get('/', function(request, response)
 {
   console.log(response.statusCode);
